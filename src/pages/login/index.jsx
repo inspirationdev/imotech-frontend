3fe6c22278da6d9afe8fec0b932d973e97f88fde
Edi6758 <ediarts7@gmail.com>
2022-12-29 23:17:48 -0300

    :sparkles: finish login page
    finish login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+
+
+import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles'
+
 const Login = () => {
     return (
         <>
-            <h1>Login</h1>
-            <Link to="/">Home</Link>
+            <Header/>
+            <Container>
+                <Column>
+                    <Title>
+                        A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas
+                    </Title>
+                </Column>
+                <div>
+                    <Wrapper>
+                        <TitleLogin>Faça seu cadastro</TitleLogin>
+                        <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                        <form action="">
+                            <Input type="email" placeholder="email"/>
+                            <Input type="password" placeholder="senha"/>
+                            <Button title="Entrar" variant='secondary'/>
+                        </form>
+                        <Row>
+                            <EsqueciText>Esqueci minha senha</EsqueciText>
+                            <CriarText>Criar conta</CriarText>
+                        </Row>
+                    </Wrapper>
+                </div>
+            </Container>
         </>
     )
 }
